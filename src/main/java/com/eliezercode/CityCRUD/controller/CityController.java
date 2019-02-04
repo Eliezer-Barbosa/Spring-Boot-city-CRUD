d764be653f6c288afced0e59f02c326cd8355c0c
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-04 23:26:59 +0000

    created method index in class CityController

--- src/main/java/com/eliezercode/CityCRUD/controller/CityController.java
+++ src/main/java/com/eliezercode/CityCRUD/controller/CityController.java
@@ -2,6 +2,8 @@ package com.eliezercode.CityCRUD.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.eliezercode.CityCRUD.repository.CityRepository;
@@ -15,6 +17,12 @@ public class CityController {
 	
 	public CityController(CityRepository cityRepository) {
 		this.cityRepository = cityRepository; 
+	}
+	
+	@GetMapping // http://localhost:8080/cities
+	public String index(Model model) {
+		model.addAttribute("cities", this.cityRepository.findAll());
+		return "cities/index";
 	}
 
 }
