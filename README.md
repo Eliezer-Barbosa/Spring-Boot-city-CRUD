f946c45224f58497f773249fd73219933f237429
Eliezer Barbosa <eliezer1789j@gmail.com>
2019-02-04 20:24:24 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Spring-Boot-city-CRUD
---
+++ src/main/java/com/eliezercode/CityCRUD/CityCrudApplication.java
@@ -0,0 +1,14 @@
+package com.eliezercode.CityCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityCrudApplication.class, args);
+	}
+
+}
+
